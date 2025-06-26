8c7047d6cceb8030625e567be0d9ff7829a40794
Thiago de Oliveira Botelho <38995115+thiagobotelho@users.noreply.github.com>
2025-06-26 11:37:39 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
+import os
 
 app = Flask(__name__)
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 @app.route("/")
 def index():
@@ -11,12 +16,12 @@ def index():
         client_ip = x_forwarded_for
         lb_type = "LB do tipo L7"
         icon = "🌐"
-        color = "#007acc"  # Azul
+        color = "#007acc"
     else:
         client_ip = remote_addr
         lb_type = "LB do tipo L4"
         icon = "🧱"
-        color = "#cc0000"  # Vermelho
+        color = "#cc0000"
 
     app.logger.info(f"{lb_type} | IP de origem: {client_ip} | remote_addr: {remote_addr} | X-Forwarded-For: {x_forwarded_for}")
 
@@ -25,16 +30,13 @@ def index():
       <head>
         <title>IP de Origem</title>
       </head>
-      <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 80px;">
-        <h1 style="color: {color}; font-size: 2.5em;">{icon} {lb_type}</h1>
-        <h2 style="margin-top: 40px;">IP de origem:</h2>
-        <p style="font-size: 2em; font-weight: bold;">{client_ip}</p>
-        <hr style="margin-top:60px; width: 40%;">
-        <p style="color: gray; font-size: 0.9em;">client-ip-api powered by Flask</p>
+      <body style='font-family: Arial, sans-serif; text-align: center; margin-top: 80px;'>
+        <h1 style='color: {color}; font-size: 2.5em;'>{icon} {lb_type}</h1>
+        <h2 style='margin-top: 40px;'>IP de origem:</h2>
+        <p style='font-size: 2em; font-weight: bold;'>{client_ip}</p>
+        <hr style='margin-top:60px; width: 40%;'>
+        <p style='color: gray; font-size: 0.9em;'>client-ip-api powered by Flask</p>
       </body>
     </html>
     """
     return html, 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
